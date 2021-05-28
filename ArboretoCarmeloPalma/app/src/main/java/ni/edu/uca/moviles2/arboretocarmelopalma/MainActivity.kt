9485c24b41f4445600968e0a0796779c4bcd1782
jessicaaviles8835 <jessica.aviles8835@est.uca.edu.ni>
2021-05-28 10:40:26 -0500

    Agregar eventos al menu
    Agregar eventos para tener el menu principal

--- ArboretoCarmeloPalma/app/src/main/java/ni/edu/uca/moviles2/arboretocarmelopalma/MainActivity.kt
+++ ArboretoCarmeloPalma/app/src/main/java/ni/edu/uca/moviles2/arboretocarmelopalma/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_trees, R.id.nav_services), drawerLayout)
+                R.id.nav_home, R.id.nav_trees, R.id.nav_services, R.id.nav_events), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
